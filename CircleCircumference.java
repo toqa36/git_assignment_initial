773057c308b37094c77d9f49a986b343f96a36f1
somiaabdulaziz10 <36263889+somiaabdulaziz10@users.noreply.github.com>
2020-02-28 00:28:09 +0200

    Add files via upload

---
+++ CircleCircumference.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package circlecircumference;
+import java.util.Scanner;
+/**
+ *
+ * @author Dell
+ */
+public class CircleCircumference {
+        static Scanner sc = new Scanner(System.in);
+   public static void main(String args[])
+   {
+      System.out.print("Enter the radius: ");
+     
+      double radius = sc.nextDouble();
+ 
+     
+      double circumference= Math.PI * 2*radius;
+      System.out.println( "The circumference of the circle is:"+circumference) ;
+   }
+    
+}
