c4cf4131f14e61464d0a566accae2017edf9ab90
toqa36 <58927131+toqa36@users.noreply.github.com>
2020-02-28 15:11:20 +0200

    Revert "20170254 add the SumSeries"

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,7 @@ public class Main {
 	private static ISubscriber subscribers [] = {
 			new SimpleSubscriber(),
 			new ReallySimpleSubscriber(),
-			new SphereCircumference(),
-			new SumSeriesSubscriber()
+			new SphereCircumference()
 	};
 	public static void main(String[] args) {
 		Topic mathTopic = new Topic();
