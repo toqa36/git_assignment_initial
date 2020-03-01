33834a2967c2b7a98bcc16a5cb8d17b9b244d128
somiaabdulaziz10 <36263889+somiaabdulaziz10@users.noreply.github.com>
2020-03-01 18:49:43 +0200

    Update CircleCircumference.java

--- CircleCircumference.java
+++ CircleCircumference.java
@@ -21,13 +21,7 @@ public class CircleCircumference {
  static Scanner sc = new Scanner(System.in);
    public static void main(String args[])
    {
-      System.out.print("Enter the radius: ");
-     
-      double radius = sc.nextDouble();
- 
-     
-     CircleCircumference(radius);
-    
+      
    }
     
 }
